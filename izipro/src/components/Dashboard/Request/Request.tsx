0df1bbf2e6adade90e65a365145c3ebfed098f53
alyweb66 <alyweb66@gmail.com>
2024-03-27 09:25:51 +0100

    add media input

--- izipro/src/components/Dashboard/Request/Request.tsx
+++ izipro/src/components/Dashboard/Request/Request.tsx
@@ -18,14 +18,15 @@ type CategoryPros = {
 
 type jobProps = {
 	id: number;
-    name: string;
-    description: string;
+	name: string;
+	description: string;
 }
 
 type LocationProps = {
 	lat: number | null;
 	lng: number | null;
 }
+
 
 function Request() {
 	//state
@@ -36,6 +37,10 @@ function Request() {
 	const [descriptionRequest, setDescriptionRequest] = useState('');
 	const [errorMessage, setErrorMessage] = useState('');
 	const [successMessage, setSuccessMessage] = useState('');
+	// file upload
+	const [file, setFile] = useState<File[]>([]);
+	const [fileError, setFileError] = useState('');
+	console.log('file', file);
 	// map
 	const [radius, setRadius] = useState(0); // Radius in meters
 	const [location, setLocation] = useState<LocationProps>({ lng: null, lat: null });
@@ -43,7 +48,7 @@ function Request() {
 	const [map, setMap] = useState<mapboxgl.Map | null>(null);
 	const [zoom, setZoom] = useState(10);
 
-	
+
 	//store
 	const id = userDataStore((state) => state.id);
 	const address = userDataStore((state) => state.address);
@@ -52,17 +57,17 @@ function Request() {
 
 	// mutation
 	const [createRequest, { error: requestError }] = useMutation(REQUEST_MUTATION);
-	
+
 	// fetch categories 
-	const { error: categoryError, data: categoriesData} = useQuery(GET_JOB_CATEGORY);
+	const { error: categoryError, data: categoriesData } = useQuery(GET_JOB_CATEGORY);
 	if (categoryError) {
 		throw new Error('Error while fetching categories data');
 	}
 
 	// fetch jobs
-	const { error: jobError, data: jobData} = useQuery(GET_JOBS_BY_CATEGORY,
+	const { error: jobError, data: jobData } = useQuery(GET_JOBS_BY_CATEGORY,
 		{
-			variables:{
+			variables: {
 				categoryId: Number(selectedCategory)
 			},
 			skip: !selectedCategory
@@ -71,7 +76,37 @@ function Request() {
 	if (jobError) {
 		throw new Error('Error while fetching jobs');
 	}
+	// file upload
+	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const files = event.target.files;
+		const maxFileSize = 1048576; // 1MB in bytes
+		// filter pdf files that are too large
+		const validFiles = Array.from(files!).filter(file => {
+			if (file.name.endsWith('.pdf')) {
+				if (file.size > maxFileSize) {
+					setFileError(`File ${file.name} is too large, please select a file smaller than 1MB.`);
+				
+					return false;
+				}
+				return true;
+			}
+			return true;
+		});
 
+		if (validFiles) {
+			const urls = validFiles.map(file => URL.createObjectURL(file));
+			const fileObjects = urls.map(url => new File([url], url));
+		
+			setFile([...file, ...fileObjects]);
+		}
+	};
+
+	// remove file
+	const handleRemove = (index: number) => {
+		const newFiles = [...file];
+		newFiles.splice(index, 1);
+		setFile(newFiles);
+	};
 	// Submit request
 	const handleSubmitRequest = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
@@ -86,7 +121,7 @@ function Request() {
 
 		} else {
 			clearTimeout(timer);
-		
+
 			createRequest({
 				variables: {
 					input: {
@@ -111,8 +146,8 @@ function Request() {
 			});
 			clearTimeout(timer);
 
-		} 
-	
+		}
+
 
 		if (requestError) {
 			throw new Error('Error while creating request');
@@ -122,12 +157,12 @@ function Request() {
 	// location
 	useEffect(() => {
 
-		if ( address && city && postal_code) {
-		// transform address to coordinates with Mapbox API
+		if (address && city && postal_code) {
+			// transform address to coordinates with Mapbox API
 			const fetchGeocoding = async () => {
 				const formattedAddress = `${address}, ${postal_code} ${city}`;
 				const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(formattedAddress)}.json?access_token=pk.eyJ1IjoiYWx5d2ViIiwiYSI6ImNsdTcwM2xnazAwdHMya3BpamhmdjRvM3AifQ.V3d3rCH-FYb4s_e9fIzNxg`;
-		
+
 				const response = await fetch(url);
 				const data = await response.json();
 
@@ -158,7 +193,7 @@ function Request() {
 			} else {
 				// Geolocation is not supported
 				setError('Geolocation is not supported by your browser');
-			} 
+			}
 		}
 	}, [address, city, postal_code]);
 
@@ -187,7 +222,7 @@ function Request() {
 				},
 				paint: {
 					'circle-radius': {
-						
+
 						stops: [
 							[0, 0],
 							[15.8, radius] // Adjust the multiplier for scaling
@@ -237,16 +272,17 @@ function Request() {
 		<div className="request-container">
 			<form className="request-form" onSubmit={handleSubmitRequest}>
 				{error && <p className="error-message">{error}</p>}
-				<button 
+				<button
 					className={`urgent-button ${urgent ? 'urgent-button-active' : ''}`}
 					onClick={(event) => {
 						event.preventDefault();
-						setUrgent(!urgent);}
+						setUrgent(!urgent);
+					}
 					}
 				>Urgent</button>
-				<select 
-					className="job-select" 
-					name="job" 
+				<select
+					className="job-select"
+					name="job"
 					id="job"
 					value={selectedCategory}
 					onChange={(event) => setSelectedCategory(event.target.value)}
@@ -256,21 +292,21 @@ function Request() {
 						<option key={index} value={category.id}>
 							{category.name}
 						</option>
-							
+
 					))}
 				</select>
 				<select
-					className="category_select" 
-					name="job" 
+					className="category_select"
+					name="job"
 					id="job"
 					value={selectedJob}
-					onChange={(event)=> setSelectedJob(event.target.value)}
-				>	
+					onChange={(event) => setSelectedJob(event.target.value)}
+				>
 					<option value="">Métiers</option>
-					{jobData && jobData.category.jobs.map((job: jobProps, index: number) =>(
+					{jobData && jobData.category.jobs.map((job: jobProps, index: number) => (
 
-						<option 
-							key={index} 
+						<option
+							key={index}
 							value={job.id}
 							title={job.description}
 						>
@@ -302,7 +338,7 @@ function Request() {
 								zoom: zoom
 							}}
 							zoom={zoom}
-							style={{width: 600, height: 400}}
+							style={{ width: 600, height: 400 }}
 							mapStyle="mapbox://styles/mapbox/streets-v9"
 							onLoad={handleMapLoaded}
 							dragRotate={false}
@@ -310,27 +346,101 @@ function Request() {
 						/>
 					</>
 				)}
-				<input 
-					className="input-request" 
-					type="text" 
+				<input
+					className="input-request"
+					type="text"
 					placeholder="Titre de la demande"
 					value={titleRequest}
 					onChange={(event) => setTitleRequest(event.target.value)}
-					maxLength={50} 
+					maxLength={50}
 				/>
-
-				<textarea 
-					className="text-request" 
-					name="description" 
-					id="description" 
+				<textarea
+					className="text-request"
+					name="description"
+					id="description"
 					placeholder="Description de la demande"
 					value={descriptionRequest}
 					onChange={(event) => setDescriptionRequest(event.target.value)}
 				>
-						
 				</textarea>
 				{errorMessage && <p className="error-message">{errorMessage}</p>}
 				{successMessage && <p className="success-message">{successMessage}</p>}
+				{fileError && <p className="error-message">{fileError}</p>}
+				{file.map((file, index) =>(
+					<div key={index} style={{ position: 'relative', display: 'inline-block' }}>
+						<img 
+							style={{ width: '100px', height: '100px', objectFit: 'cover' }}
+							src={file.name} 
+							alt={`Preview ${index}`} 
+						/>
+						<div 
+							style={{ 
+								position: 'absolute', 
+								top: '0', 
+								right: '0', 
+								background: 'red', 
+								color: 'white', 
+								cursor: 'pointer' 
+							}}
+							onClick={() => handleRemove(index)}
+						>
+					X
+						</div>
+					</div>
+				))}
+				<label htmlFor="file" className="labelFile">
+					<span>
+						<svg
+							xmlSpace="preserve"
+							viewBox="0 0 184.69 184.69"
+							xmlnsXlink="http://www.w3.org/1999/xlink"
+							xmlns="http://www.w3.org/2000/svg"
+							id="Capa_1"
+							version="1.1"
+							width="60px"
+							height="60px"
+						>
+							<g>
+								<g>
+									<g>
+										<path
+											d="M149.968,50.186c-8.017-14.308-23.796-22.515-40.717-19.813
+				C102.609,16.43,88.713,7.576,73.087,7.576c-22.117,0-40.112,17.994-40.112,40.115c0,0.913,0.036,1.854,0.118,2.834
+				C14.004,54.875,0,72.11,0,91.959c0,23.456,19.082,42.535,42.538,42.535h33.623v-7.025H42.538
+				c-19.583,0-35.509-15.929-35.509-35.509c0-17.526,13.084-32.621,30.442-35.105c0.931-0.132,1.768-0.633,2.326-1.392
+				c0.555-0.755,0.795-1.704,0.644-2.63c-0.297-1.904-0.447-3.582-0.447-5.139c0-18.249,14.852-33.094,33.094-33.094
+				c13.703,0,25.789,8.26,30.803,21.04c0.63,1.621,2.351,2.534,4.058,2.14c15.425-3.568,29.919,3.883,36.604,17.168
+				c0.508,1.027,1.503,1.736,2.641,1.897c17.368,2.473,30.481,17.569,30.481,35.112c0,19.58-15.937,35.509-35.52,35.509H97.391
+				v7.025h44.761c23.459,0,42.538-19.079,42.538-42.535C184.69,71.545,169.884,53.901,149.968,50.186z"
+											fill="#010002"
+										></path>
+									</g>
+									<g>
+										<path
+											d="M108.586,90.201c1.406-1.403,1.406-3.672,0-5.075L88.541,65.078
+				c-0.701-0.698-1.614-1.045-2.534-1.045l-0.064,0.011c-0.018,0-0.036-0.011-0.054-0.011c-0.931,0-1.85,0.361-2.534,1.045
+				L63.31,85.127c-1.403,1.403-1.403,3.672,0,5.075c1.403,1.406,3.672,1.406,5.075,0L82.296,76.29v97.227
+				c0,1.99,1.603,3.597,3.593,3.597c1.979,0,3.59-1.607,3.59-3.597V76.165l14.033,14.036
+				C104.91,91.608,107.183,91.608,108.586,90.201z"
+											fill="#010002"
+										></path>
+									</g>
+								</g>
+							</g>
+						</svg>
+					</span>
+					<p>Glissez et déposez votre fichier ici ou cliquez pour sélectionner un fichier! (Format accepté : .jpg,.jpeg,.png,.pdf, pdf inférieur à 1Mo)</p>
+				</label>
+				<input 
+					id="file" 
+					className="input" 
+					name="text" 
+					type="file"
+					multiple={true} 
+					onChange={handleFileChange}
+					accept=".jpg,.jpeg,.png,.pdf" 
+				/>
+
 				<button className="request_submit" type="submit">Envoyer</button>
 			</form>
 
