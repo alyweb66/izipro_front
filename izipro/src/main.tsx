c1c9205866769d87a28b59027e254353c2291898
alyweb66 <alyweb66@gmail.com>
2024-11-28 11:31:32 +0100

    remove auth in main

--- izipro/src/main.tsx
+++ izipro/src/main.tsx
@@ -70,8 +70,6 @@ const userIdMiddleware = setContext((_, { headers }) => {
 			headers: {
 				...headers,
 				userid: id > 0 ? id : '',
-				// for openlitespeed
-				'X-Frontend-Auth': import.meta.env.VITE_AUTH_KEY,
 			},
 		};
 	}
